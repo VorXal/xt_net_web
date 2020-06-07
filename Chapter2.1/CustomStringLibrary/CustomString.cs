9f5e447a41586ef209c1a978304bfc422cc19ff6
Vorxal <alexey32101231@gmail.com>
2020-06-07 20:17:05 +0400

    Added .dll for task 2.1.1

---
+++ Chapter2.1/CustomStringLibrary/CustomString.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Task2._1._1
+{
+    public class CustomStringFromDLL
+    {
+        private char[] customString;
+        public int Length
+        {
+            get
+            {
+                return customString.Length;
+            }
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                return customString[index];
+            }
+            private set
+            {
+                customString[index] = value;
+            }
+        }
+
+
+        public CustomStringFromDLL(char[] input)
+        {
+            this.customString = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                customString[i] = input[i];
+            }
+        }
+
+        public CustomStringFromDLL(string input)
+        {
+            this.customString = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                customString[i] = input[i];
+            }
+        }
+
+        public void PrintCustomString()
+        {
+            foreach(var i in this.customString)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintCustomString(CustomStringFromDLL input)
+        {
+            foreach (var i in input.customString)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.customString.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            CustomStringFromDLL cs = (CustomStringFromDLL)obj;
+            return (this.customString == cs.customString);
+        }
+
+        public CustomStringFromDLL ConcatCustomString(CustomStringFromDLL secondCustomString)
+        {
+            int firstCustomStringLength = customString.Length;
+            CustomStringFromDLL firstCustomString = new CustomStringFromDLL(customString);
+            customString = new char[customString.Length + secondCustomString.Length];
+
+            for(int i = 0; i < firstCustomStringLength + secondCustomString.Length; i++)
+            { 
+                if(i < firstCustomStringLength)
+                {
+                    customString[i] = firstCustomString[i];
+                }
+                else
+                {
+                    customString[i] = secondCustomString[i - firstCustomString.Length];
+                }
+            }
+
+            return new CustomStringFromDLL(customString);
+        }
+
+        public static CustomStringFromDLL ConcatCustomString(CustomStringFromDLL firstCustomString, CustomStringFromDLL secondCustomString)
+        {
+            char[] customString = new char[firstCustomString.Length + secondCustomString.Length];
+
+            for (int i = 0; i < firstCustomString.Length + secondCustomString.Length; i++)
+            {
+                if (i < firstCustomString.Length)
+                {
+                    customString[i] = firstCustomString[i];
+                }
+                else
+                {
+                    customString[i] = secondCustomString[i - firstCustomString.Length];
+                }
+            }
+
+            return new CustomStringFromDLL(customString);
+        }
+
+        public int GetPositionCharInCustomString(char find)
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (this[i] == find)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int GetPositionCharInCustomString(char find, CustomStringFromDLL input)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == find)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public char[] ReturnArray()
+        {
+            return this.customString;
+        }
+
+        public static char[] ReturnArray(CustomStringFromDLL input)
+        {
+            return input.customString;
+        }
+
+        public int CountOfCharInCustomString(char find)
+        {
+            int count = 0;
+
+            foreach (var i in this.customString)
+            {
+                if (i == find)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int CountOfCharInCustomString(char find, CustomStringFromDLL input)
+        {
+            int count = 0;
+
+            foreach(var i in input.customString)
+            {
+                if(i == find)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
--- Chapter2.1/Task2.1.1/Program.cs
+++ Chapter2.1/Task2.1.1/Program.cs
@@ -1,29 +1,18 @@
 ﻿using System;
 
+
 namespace Task2._1._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CustomString cs1 = new CustomString("Hello");
-            CustomString cs2 = new CustomString("Hello!");
-            cs1.PrintCustomString();
-            cs2.PrintCustomString();
-            Console.WriteLine(cs1.Equals(cs2));
-            cs1.ConcatCustomString(cs2);
-            cs1.PrintCustomString();
-            CustomString cs3 = CustomString.ConcatCustomString(cs1, cs2);
-            cs3.PrintCustomString();
-            Console.WriteLine(CustomString.Equals(cs3, cs3));
-            CustomString.PrintCustomString(cs3);
-            Console.WriteLine(CustomString.GetPositionCharInCustomString('0', cs3));
-            Console.WriteLine(cs3.GetPositionCharInCustomString('e'));
-            char[] a = cs3.ReturnArray();
-            Console.WriteLine(a);
-            Console.WriteLine(CustomString.CountOfCharInCustomString('H', cs3));
-            Console.WriteLine(cs1[1]);
-            Console.ReadKey();
+            CustomStringFromDLL customString = new CustomStringFromDLL("This string working from DLL");
+
+            CustomString customString1 = new CustomString("This string working from class");
+
+            customString.PrintCustomString();
+            customString1.PrintCustomString();
         }
     }
 }
