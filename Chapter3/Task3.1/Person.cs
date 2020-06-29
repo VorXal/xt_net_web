d55067ac3f66d4ada321e4fa680f86726afc26f3
Vorxal <alexey32101231@gmail.com>
2020-06-29 16:09:03 +0400

    Initiate Task3.1

---
+++ Chapter3/Task3.1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task3._1
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public bool Winner { get; private set; } = true;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            string status = Winner ? "Winner" : "Loser";
+            return $"Name: {Name}, Status: {status} ";
+        }
+        public void ChangeStatus()
+        {
+            Winner = false;
+        }
+
+    }
+}
---
+++ Chapter3/Task3.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task3._1
+{
+    class Program
+    {
+        public static void GetInfo(Person[] people, int round)
+        {
+            Console.WriteLine($"Round: {round}");
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+        public static int GetNextLoserId(Person[] people, int step)
+        {
+            int indexNow = 1;
+            while (true)
+            {
+                if (indexNow >= people.Length)
+                {
+                    indexNow -= people.Length;
+                }
+                if (people[indexNow + step].Winner)
+                    return indexNow + step;
+                else
+                {
+                    indexNow += step;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int step = 3;
+            Person person = new Person("Alexey");
+            Person person1 = new Person("Sergey");
+            Person person2 = new Person("Ivan");
+            Person person3 = new Person("Alexandr");
+            Person person4 = new Person("Mila");
+            Person person5 = new Person("Elena");
+            Person[] people = new Person[] { person, person1, person2, person3, person4, person5};
+
+
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            int losers = 0;
+            
+
+            while(losers < people.Length - 1)
+            {
+                people[GetNextLoserId(people, step)].ChangeStatus();
+                losers++;
+                GetInfo(people, losers);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
