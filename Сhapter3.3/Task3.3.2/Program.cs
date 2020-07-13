8e96537f865203f0de833a398dbc9b4233348b91
VorXal <alexey32101231@gmail.com>
2020-07-13 22:39:04 +0400

    Release Task3.3.2

---
+++ Сhapter3.3/Task3.3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This app can help you to calculate type of string.");
+            Console.WriteLine("For EXIT please insert 'EXIT'");
+            while (true)
+            {
+                Console.Write("Please insert your string: ");
+                string temp = Console.ReadLine();
+                if (temp == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Your string have type: {temp.GetStringType()}");
+                }
+            }
+        }
+    }
+}
---
+++ Сhapter3.3/Task3.3.2/StringExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task3._3._2
+{
+    public static class StringExtension
+    {
+        public enum StringType
+        {
+            None,
+            Russian,
+            English,
+            Number,
+            Mixed
+        }
+
+        public static StringType GetStringType(this string inputString)
+        {
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                if (StringIsEnglish(inputString))
+                {
+                    return StringType.English;
+                }
+                else if (StringIsRussian(inputString))
+                {
+                    return StringType.Russian;
+                }
+                else if (StringIsDigit(inputString))
+                {
+                    return StringType.Number;
+                }
+                else if (StringIsMixed(inputString))
+                {
+                    return StringType.Mixed;
+                }
+                else
+                {
+                    return StringType.None;
+                }
+            }
+            else
+            {
+                return StringType.None;
+            }
+        }
+
+        private static bool StringIsEnglish(string str)
+        {
+            str = str.ToLower();
+            return Regex.IsMatch(str, @"\b([a-z])+\b");
+        }
+
+        private static bool StringIsRussian(string str)
+        {
+            str = str.ToLower();
+            return Regex.IsMatch(str, @"\b([а-яё])+\b");
+        }
+
+        private static bool StringIsMixed(string str)
+        {
+            str = str.ToLower();
+            return Regex.IsMatch(str, @"([a-zA-Z]|[а-яА-Я]|[0-9])+\w");
+        }
+
+        private static bool StringIsDigit(string str)
+        {
+            str = str.ToLower();
+            return Regex.IsMatch(str, @"\b[\d]+\b");
+        }
+    }
+}
